66d4d3e0baca6edb627e611c67fca8033598ef29
TrocyukS96 <trotzuk.stanislav@gmail.com>
2021-06-28 19:59:13 +0300

    first commit

---
+++ src/blocks/header/Header.js
@@ -0,0 +1,13 @@
+// import './App.css';
+
+
+
+function Header() {
+  return (
+    <header className="header">
+         <h1>Моя первая запись</h1>
+    </header>
+  );
+}
+
+export default Header;
